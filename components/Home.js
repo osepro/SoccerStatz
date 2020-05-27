c87c252a0ce9033b76fda9544703da617715a920
Ose Agbadu <v-osea@zg05643.zillow.local>
2020-05-26 21:58:04 -0400

    fix: homescreen midify

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,95 @@
 import React, { Component, Fragment } from "react"
-import { View, Text, TextInput, KeyboardAvoidingView, StyleSheet, TouchableOpacity, StatusBar, ImageBackground } from "react-native"
+import { View, Text, TextInput, KeyboardAvoidingView, StyleSheet, Button, TouchableOpacity, StatusBar, ImageBackground } from "react-native"
 import { connect } from "react-redux";
-import { white, orange, green, black, gray } from "../utils/colors";
+import { white, orange, green, black, gray, blue } from "../utils/colors";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
-import Menu from "./Menu";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
 const image = '../assets/soccerstazbg.png'
+
+function HomeScreen({ navigation }) {
+	return (
+		<KeyboardAvoidingView behavior="padding" style={styles.container}>
+			<View style={styles.statusBar}>
+				<StatusBar barStyle="light-content" />
+				<TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+					<FontAwesome name='navicon' size={30} color={black} />
+				</TouchableOpacity>
+			</View>
+			<View style={styles.row}>
+				<View style={styles.imageHolder}>
+					<Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+					<Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
+					<ImageBackground source={require(image)} style={styles.image}></ImageBackground>
+				</View>
+			</View>
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<Button
+					onPress={() => navigation.navigate('Notifications')}
+					title="Go to notifications"
+				/>
+			</View>
+		</KeyboardAvoidingView>
+	);
+}
+
+function NotificationsScreen({ navigation }) {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<FontAwesome name='plus-square' size={30} color={gray} />
+			<Button onPress={() => navigation.goBack()} title="Go back home" />
+		</View>
+	);
+}
+
+function addPlayer() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<FontAwesome name='plus-square' size={30} color={blue} />
+			<Button onPress={() => navigation.goBack()} title="Add Player" />
+		</View>
+	)
+}
+
+function viewPlayer() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Button onPress={() => navigation.goBack()} title="viewPlayer" />
+		</View>
+	)
+}
+
+function deletePlayer() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Button onPress={() => navigation.goBack()} title="Delete Player" />
+		</View>
+	)
+}
+
+function logOut() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Button onPress={() => navigation.goBack()} title="Sign Out" />
+		</View>
+	)
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+	return (
+		<Drawer.Navigator initialRouteName="Home">
+			<Drawer.Screen name="Home" component={HomeScreen} />
+			<Drawer.Screen name="Add Player" component={addPlayer} />
+			<Drawer.Screen name="View Player" component={viewPlayer} />
+			<Drawer.Screen name="Delete Player" component={deletePlayer} />
+			<Drawer.Screen name="LogOut" component={logOut} />
+		</Drawer.Navigator>
+	);
+}
+
 
 class Home extends Component {
 	state = {
@@ -15,15 +99,8 @@ class Home extends Component {
 
 	render() {
 		const { login } = this.props;
-
 		return (
-			<KeyboardAvoidingView behavior="padding" style={styles.container}>
-				<View style={styles.row}>
-					<View style={styles.imageHolder}>
-						<ImageBackground source={require(image)} style={styles.image}></ImageBackground>
-					</View>
-				</View>
-			</KeyboardAvoidingView>
+			<MyDrawer />
 		)
 	}
 }
@@ -36,9 +113,14 @@ const mapStateToProps = ({ login }) => {
 
 const styles = StyleSheet.create({
 	container: {
-		padding: 20,
 		backgroundColor: white,
 		flex: 1
+	},
+	statusBar: {
+		backgroundColor: green,
+		height: 120,
+		justifyContent: "center",
+		paddingLeft: 10,
 	},
 	row: {
 		flex: 1,
