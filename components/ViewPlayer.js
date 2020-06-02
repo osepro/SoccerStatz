5db3ade0714f7b5cff0aec1393d2107f0bfbfe97
Ose Agbadu <v-osea@zg05643.zillow.local>
2020-06-01 22:29:43 -0400

    feat: view player

--- components/ViewPlayer.js
+++ components/ViewPlayer.js
@@ -10,15 +10,15 @@ import Moment from 'moment';
 const DATA = [
 	{
 		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-		title: 'First Item',
+		title: 'James Dune',
 	},
 	{
 		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-		title: 'Second Item',
+		title: 'Avil Larry',
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d72',
-		title: 'Third Item',
+		title: 'Steven Cov',
 	},
 
 ];
@@ -49,8 +49,9 @@ class ViewPlayer extends Component {
 			<SafeAreaView style={styles.container}>
 				<FlatList
 					data={DATA}
-					renderItem={({ item }) => <Item title={item.title} />}
+					renderItem={({ item }) => <Item title={item.title} style={styles.list} />}
 					keyExtractor={item => item.id}
+					style={styles.listItem}
 				/>
 			</SafeAreaView>
 		)
@@ -65,13 +66,21 @@ function mapStateToProps({ login }) {
 
 const styles = StyleSheet.create({
 	container: {
-		padding: 40,
+		padding: 20,
 		backgroundColor: white,
 		flex: 1
 	},
 	row: {
 		flex: 1,
 		justifyContent: "center"
+	},
+	listItem: {
+		padding: 10
+	},
+	list: {
+		padding: 40,
+		borderBottomColor: black,
+		borderWidth: 3
 	},
 	statusBar: {
 		flexDirection: "row",
